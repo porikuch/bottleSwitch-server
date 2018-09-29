8ec1748fd2ca5d5b9db26ba22127349ca7a4a85f
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-09-29 21:11:50 +0900

    Fix little

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -2,9 +2,9 @@ class LoginsController < ApplicationController
   def create
     user = User.find_by(name: params[:name])
     if user.nil? or user.password != Digest::SHA256.hexdigest(params[:password]) then
-      render json: {status: 'false'}
+      render json: {status: false}
     else
-      render json: {status: 'true', id: user.id, name: user.name, nickname: user.nickname}
+      render json: {status: true, id: user.id, name: user.name, nickname: user.nickname}
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
   def create
     if User.find_by(name: params[:name]) != nil then
-      render json: {status: 'false', message: 'ユーザ名が既に使用されています'}
+      render json: {status: false, message: 'ユーザ名が既に使用されています'}
     else
       User.create(name: params[:name],
                   password: Digest::SHA256.hexdigest(params[:password]),
                   nickname: params[:nickname]
                  )
-      render json: {status: 'true'}
+      render json: {status: true}
     end
   end
 end
