506771c5303026d374a9ba19c60df82cad38862d
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-09-29 18:46:56 +0900

    Implements login

---
+++ app/controllers/logins_controller.rb
@@ -0,0 +1,11 @@
+class LoginsController < ApplicationController
+  def create
+    user = User.find_by(name: params[:name])
+    if user.nil? or user.password != Digest::SHA256.hexdigest(params[:paswword]) then
+      render json: {status: 'false'}
+    else
+      render json: {status: 'true', id: user.id, name: user.name, nickname: user.nickname}
+    end
+  end
+
+end
