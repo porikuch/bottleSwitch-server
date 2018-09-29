401fe0e08aea4ff5e872a64b44e3c785728e378e
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-09-29 20:44:12 +0900

    Implement sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  def create
+    if User.find_by(name: params[:name]) != nil then
+      render json: {status: 'ユーザ名が既に使用されています'}
+    else
+      User.create(name: params[:name],
+                  password: Digest::SHA256.hexdigest(params[:password]),
+                  nickname: params[:nickname]
+                 )
+    end
+  end
 end
