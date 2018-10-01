710edc47e3c88430ea8332a1d98baa948913c563
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-10-01 16:34:57 +0900

    Change function create bottle

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -1,8 +1,9 @@
 class BottlesController < ApplicationController
   def create
+    id = User.find_by(id: params[name]).id
     Bottle.create(title: params[:title],
                   body: params[:body],
-                  created_user_id: params[:id]
+                  created_user_id: id
                  )
     render json: {status: true}
   end
