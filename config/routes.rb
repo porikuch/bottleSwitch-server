a65b69f2d9739c980d45414ef78ac66c30009ad0
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-10-01 16:05:47 +0900

    Chage route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users do
-    resources :bottles
-  end
-
+  resources :users
+  resources :bottles
   resources :logins
 end
