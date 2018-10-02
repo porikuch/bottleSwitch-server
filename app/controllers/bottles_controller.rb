e8cb35fb3c52513b9dc4b7a6d778b446f1d29e0f
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-10-02 20:07:50 +0900

    Implemnts function index bottle

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -9,5 +9,9 @@ class BottlesController < ApplicationController
   end
 
   def index
+    find_user_id = (User.find_by(name: params[:name])).id.to_i
+    bottle_index = Bottle.where(created_user_id: find_user_id)
+
+    render json: {bottles: bottle_index}
   end
 end
