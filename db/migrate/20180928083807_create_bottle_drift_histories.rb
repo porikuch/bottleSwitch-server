8f151876485a83be5cf0bfa902108d613f414680
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-11-10 00:19:53 +0900

    Change some relate drift point

--- db/migrate/20180928083807_create_bottle_drift_histories.rb
+++ db/migrate/20180928083807_create_bottle_drift_histories.rb
@@ -2,8 +2,7 @@ class CreateBottleDriftHistories < ActiveRecord::Migration[5.1]
   def change
     create_table :bottle_drift_histories do |t|
       t.integer :bottle_id, null: false
-      t.float :place_lat, null: false
-      t.float :place_lng, null: false
+      t.integer :drift_point_id, null: false
 
       t.timestamps
     end
